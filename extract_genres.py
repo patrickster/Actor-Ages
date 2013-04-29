7e243cfa38eedbe5edc45f3b398825bf882ddea5
patrick <chalkypirate@gmail.com>
2013-04-28 21:29:53 -0700

    Initial commit.

---
+++ extract_genres.py
@@ -0,0 +1,38 @@
+import sys
+import os
+import csv
+import re
+from BeautifulSoup import BeautifulSoup
+
+
+def get_genres(film_id):
+  """
+  Returns a list of genres ascribed to an IMDb title.
+
+  Args:
+    film_id: the id of the IMDb title.
+  """
+  genre_re = re.compile('genre/(.+)\?ref')
+  with open('html/%s.html' % film_id) as f:
+    soup = BeautifulSoup(f.read())
+    try:
+      genres = [span.text for span in soup.findAll('span', {'itemprop' : 'genre'})]
+    except:
+      genres = []
+  return genres
+
+
+def main():
+  files = [f for f in os.listdir('html') if '.html' in f]
+  film_ids = [f.replace('.html', '') for f in files]
+  with open('data/genres.csv', 'w') as f:
+    w = csv.writer(f)
+    for film_id in film_ids:
+      genres = get_genres(film_id)
+      row = [film_id] + genres
+      print row
+      w.writerow(row)
+
+
+if __name__ == '__main__':
+  sys.exit(main())

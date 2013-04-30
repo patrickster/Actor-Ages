2f6b38d9b27e39b81e9a033c6bd38751dd673e30
patrick <chalkypirate@gmail.com>
2013-04-29 23:57:37 -0700

    Cleaned up code.

--- extract_genres.py
+++ extract_genres.py
@@ -2,6 +2,7 @@ import sys
 import os
 import csv
 import re
+from functions import *
 from BeautifulSoup import BeautifulSoup
 
 
@@ -11,9 +12,10 @@ def get_genres(film_id):
 
   Args:
     film_id: the id of the IMDb title.
+
   """
   genre_re = re.compile('genre/(.+)\?ref')
-  with open('html/%s.html' % film_id) as f:
+  with open('%s%s.html' % (HTML_DIR, film_id)) as f:
     soup = BeautifulSoup(f.read())
     try:
       genres = [span.text for span in soup.findAll('span', {'itemprop' : 'genre'})]
@@ -25,7 +27,7 @@ def get_genres(film_id):
 def main():
   files = [f for f in os.listdir('html') if '.html' in f]
   film_ids = [f.replace('.html', '') for f in files]
-  with open('data/genres.csv', 'w') as f:
+  with open('%s/genres.csv' % DATA_DIR, 'w') as f:
     w = csv.writer(f)
     for film_id in film_ids:
       genres = get_genres(film_id)
